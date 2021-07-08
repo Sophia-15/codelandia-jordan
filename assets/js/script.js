033bb120fa53ee800a7ae5b9b7f3fdaaa1b97b4e
Sophia Gallindo <67246528+Sophia-15@users.noreply.github.com>
2021-07-08 14:57:57 -0300

    Update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 75 * i);
+});
+}
+
+
+const jordanText = document.querySelector('#typed');
+typeWriter(jordanText);
